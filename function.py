1b98fcf53b652e3d7e1bdf8843a29a50f30a69ce
can <gurcaglarogulcan@gmail.com>
2025-02-04 23:34:32 +0300

    initial commit

---
+++ function.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from function2 import sayHello
+import os
+
+if __name__ == '__main__':
+	sayНello()
---
+++ function2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def sayНello():
+    print("Goodbye, World!")
+
+def sayHello():
+    print("Hello, World!")
+    
+if __name__ == '__main__':
+	sayНello()
